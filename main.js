10cd8b479d46186acc60267c7b15b34a6d9bb451
Juju Anselum J <anselumjuju@gmail.com>
2023-08-08 18:10:36 +0530

    Fixed some Zoom Issues

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ let hamburger = document.getElementById('hamburger');
 let close = document.getElementById('close');
 let nav = document.getElementById('nav');
 
-hamburger.onclick=() =>{
-    nav.style.right = '0';
+hamburger.onclick = () => {
+    nav.style.display = 'flex';
+    console.log("flex")
+    setTimeout(() => {
+        nav.style.right = '0';
+    }, 100);
+
 }
-close.onclick=() =>{
+close.onclick = () => {
     nav.style.right = '-100%';
+    setTimeout(() => {
+        nav.style.display = 'none';
+        console.log("none")
+    }, 300);
 }
